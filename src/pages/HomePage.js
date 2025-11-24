76ee65edf86996caa4236ae28f2e0d9f5fcc5762
João Brocca <162700492+JoaoPedroB08@users.noreply.github.com>
2025-11-23 23:45:07 -0300

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+import BannerFluxity from "../components/layout/Bannerfluxity";
+import TextoSobreUm from "../components/layout/TextoSobreUm";
+import TextoSobreDois from "../components/layout/TextoSobreDois";
+
+const HomePage = () => {
+    return (
+        <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", width: "100%" }}>
+            <Navbar />
+
+            <main style={{ flex: 1, backgroundColor: "#ffffffff", width: "100%" }}>
+                <BannerFluxity />
+                <TextoSobreUm/>
+                <TextoSobreDois/>
+            </main>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default HomePage;
